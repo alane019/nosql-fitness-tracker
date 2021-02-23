469aded7dd3bd3a7728a62fa6502ffb38bccb8b4
Adam L <alane019@gmail.com>
2021-02-22 23:01:51 -0500

    express middleware

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ var  express = require("express");
 
 var app = express();
 
+
+
+//middleware 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(bodyParser.json());
+
+
+app.use(express.static("public"));
