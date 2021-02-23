b6d8e661313730dff287cccf0242e73e5406931f
Adam L <alane019@gmail.com>
2021-02-23 00:00:59 -0500

    update public/workout.js

--- public/workout.js
+++ public/workout.js
@@ -1,4 +1,4 @@
-gasync function initWorkout() {
+async function initWorkout() {
   const lastWorkout = await API.getLastWorkout();
   console.log("Last workout:", lastWorkout);
   if (lastWorkout) {
