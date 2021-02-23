c8da5834eb6a7000c819eb9c17f516c22ef5ba56
Adam L <alane019@gmail.com>
2021-02-22 23:41:29 -0500

    add db connection string, add logger,  update server arrangement

--- app.js
+++ app.js
@@ -1,18 +1,47 @@
 
 // app server and controller 
 
-var  express = require("express");
+const  express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const app = express();
 
-var app = express();
+app.use(logger("dev"));
 
+
+// PORT 
+const PORT = process.env.PORT || 3000; 
+
+
+// * add path to schema files in models dir
+// ... 
 
 
 //middleware 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 
+//mongoose connection string 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+
+//serve public files
 app.use(express.static("public"));
+
+// * api routes
+//  ....
+
+
+
+
+// app listen
+app.listen(PORT, () => {
+
+    if(PORT === process.env.PORT){
+        console.log(`App running PORT: ${PORT}`);
+    } else {
+        console.log(`App listening at http://localhost:${PORT}`);
+    }
+  
+});
