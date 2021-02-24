6deae98a3c5be26db257a8b9c456447e7a4a7c20
Adam L <alane019@gmail.com>
2021-02-23 23:25:44 -0500

    update server file arrangement

--- app.js
+++ app.js
@@ -1,72 +1,29 @@
 // app server and controller 
-
 const  express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-
-// require mongoose schema file 
-var db = require("./workout");
-
 const app = express();
+const logger = require("morgan");
+const PORT = process.env.PORT || 3000;
+let mongoose = require("mongoose"); 
 
 app.use(logger("dev"));
-
-
-// PORT 
-const PORT = process.env.PORT || 3000; 
+app.use(logger("tiny"));
 
 //middleware 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-// connection string 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-
-//serve public files
-app.use(express.static("public"));
-
-// api routes
-
-//GET: get workout (the main API route: "/api/workouts" )
-app.get("api/workouts", (req, res) => {
-    db.Workout.find({})
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-    .catch((err) => {
-        res.json(err);
-    });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true
 });
 
-//PUT:  Edit workouts ("/api/workouts/:id")
-// mongo update method has additional "operator keys"
-app.put("/api/workouts/:id", (req, res) => {
-    db.Workout.updateOne({
-     _id: req.params.id
-    }, {
-      $push: {
-        exercises: req.body
-      }
-    })
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
-    })
-      .catch((err) => {
-        res.json(err)
-      });
-  });
+// include api routes
+var controller = require("./controller/workout_controller.js");
+controller(app);
+//require("./controller/workout_controller.js")(app);
 
-//POST:  add  a new workout
-
-//GET(`/api/workouts/`)  >>> get workouts in a range
-
-// ...........
-
-// app listen
+// app listen 
 app.listen(PORT, () => {
-
-        console.log(`App listening at http://localhost:${PORT}`);
-  
+  console.log(`App listening at http://localhost:${PORT}`);
 });
 
